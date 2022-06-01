2aa8ce896a9db36a8a0abe4213ba7b072eb4ba1c
Ruben Quadros <ruben@sharechat.co>
2022-06-01 23:17:07 +0530

    Read pr details

--- update-changelog.main.kts
+++ update-changelog.main.kts
@@ -3,16 +3,52 @@
 @file:CompilerOptions("-jvm-target", "11")
 @file:Repository("https://repo.maven.apache.org/maven2")
 
-val versionFile = File("/module-one/publish.properties")
-val properties = Properties()
-properties.load(FileInputStream(versionFile))
-val version = properties["version"].toString()
-println("version: $version")
+import java.io.*
+import java.util.*
 
-if (version.contains("suffix")) {
-    //not for release
-    println("Not release")
-} else {
-    //should be released
-    println("release")
+val prDetails = getPRDetails()
+val isRelease = isReleaseBuild()
+
+fun getPRDetails(): PRDetails {
+    val message = "github.event.head_commit.message".runCommandWithRedirect()
+    val prNumber = "github.event.number".runCommandWithRedirect()
+
+    println("message: $message, prNumber: $prNumber")
+
+    return PRDetails(message = message, number = prNumber)
 }
+
+fun isReleaseBuild(): Boolean {
+    val versionFile = File("module-one/publish.properties")
+    val properties = Properties()
+    properties.load(FileInputStream(versionFile))
+    val version = properties["version"].toString()
+    println("version: $version")
+
+    return if (version.contains("suffix")) {
+        //not for release
+        println("Not release")
+        true
+    } else {
+        //should be released
+        println("release")
+        false
+    }
+}
+
+fun writeChangeLog() {
+
+}
+
+fun String.runCommandWithRedirect(dir: File? = null) =
+    ProcessBuilder("/bin/sh", "-c", this)
+        .redirectErrorStream(true)
+        .inheritIO()
+        .directory(dir)
+        .start()
+        .waitFor()
+
+data class PRDetails(
+    val message: Int,
+    val number: Int
+)
