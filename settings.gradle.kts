600d8c5b58ea4453ba58c14e173170de03d8463b
Ruben Quadros <ruben@sharechat.co>
2022-06-01 20:06:07 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "what-changed"
+include(":module-one")
+include(":module-two")
