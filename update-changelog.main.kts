aad792fe30b960f497ad6777d61e258a8026d509
Ruben Quadros <ruben@sharechat.co>
2022-06-01 21:58:24 +0530

    read version

--- update-changelog.main.kts
+++ update-changelog.main.kts
@@ -3,4 +3,16 @@
 @file:CompilerOptions("-jvm-target", "11")
 @file:Repository("https://repo.maven.apache.org/maven2")
 
-print("Running")
+val versionFile = File("/module-one/publish.properties")
+val properties = Properties()
+properties.load(FileInputStream(versionFile))
+val version = properties["version"].toString()
+println("version: $version")
+
+if (version.contains("suffix")) {
+    //not for release
+    println("Not release")
+} else {
+    //should be released
+    println("release")
+}
